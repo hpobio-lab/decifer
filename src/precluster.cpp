5dcdf201559543f03f831c42c20713578a8c8033
Mohammed El-Kebir <melkebir@cs.brown.edu>
2019-06-08 13:49:27 -0500

    Pre clustering

---
+++ src/precluster.cpp
@@ -0,0 +1,100 @@
+/*
+ * precluster.cpp
+ *
+ *  Created on: 6-jun-2019
+ *      Author: M. El-Kebir
+ */
+
+#include "precluster.h"
+
+PreCluster::PreCluster(const ReadMatrix& R)
+  : _R(R)
+  , _nrClusters()
+  , _preClustering(R.getNrCharacters(), -1)
+{
+}
+
+void PreCluster::run(int nrSegments,
+                     Solver::ClusterStatisticType statType,
+                     double precisionBetaBin,
+                     int nrThreads,
+                     int timeLimit,
+                     bool verbose,
+                     int memoryLimit)
+{
+  IntMatrix partition = _R.partition();
+  
+  std::cerr << "Precluster: " << partition.size() << " groups of SNVs with identical copy numbers" << std::endl;
+  int idx = 0;
+  for (const IntVector& snvIndices : partition)
+  {
+    ++idx;
+    std::cerr << "Precluster: clustering group " << idx << "/" << partition.size() << " with " << snvIndices.size() << " SNVs" << std::endl;
+    run(snvIndices, nrSegments, statType, precisionBetaBin,
+        nrThreads, timeLimit, verbose, memoryLimit);
+  }
+  
+  _invPreClustering = IntMatrix(_nrClusters);
+  for (int i = 0; i < _R.getNrCharacters(); ++i)
+  {
+    _invPreClustering[_preClustering[i]].push_back(i);
+  }
+}
+
+void PreCluster::run(const IntVector& snvIndices,
+                     int nrSegments,
+                     Solver::ClusterStatisticType statType,
+                     double precisionBetaBin,
+                     int nrThreads,
+                     int timeLimit,
+                     bool verbose,
+                     int memoryLimit)
+{
+  ReadMatrix R(_R.downSampleCharacters(snvIndices));
+  
+  const int nrObservations = R.getNumberOfObservations();
+  
+  double min_bic = std::numeric_limits<double>::max();
+  IntVector solZ;
+  int selected_k = -1;
+  
+  const size_t max_k = std::min((size_t)10, snvIndices.size());
+  for (int k = 1; k <= max_k; ++k)
+  {
+    int nrParameters = R.getNrSamples() * (k+1);
+    {
+      Solver solver(R, k, 0, statType, precisionBetaBin, false);
+      solver.init();
+      
+      for (int i = 0; i < R.getNrCharacters(); ++i)
+      {
+        nrParameters += k * solver.getScriptT(i).size() * k;
+      }
+    }
+    
+    HardPreClusterIlpAlg solver(R, k, nrSegments, statType, precisionBetaBin, true);
+    solver.init();
+    solver.solve(nrThreads, timeLimit, verbose, memoryLimit);
+    
+    double b = log(nrObservations) * nrParameters - 2 * solver.getLogLikelihood();
+    std::cerr << "k = " << k << " -- Log likelihood " << solver.getLogLikelihood() << " -- BIC " << b << std::endl;
+    
+    if (b < min_bic)
+    {
+      min_bic = b;
+      solZ = solver.getSolZ();
+      selected_k = k;
+    }
+    else
+    {
+      break;
+    }
+  }
+  
+  for (int i = 0; i < snvIndices.size(); ++i)
+  {
+    int org_i = snvIndices[i];
+    _preClustering[org_i] = _nrClusters + solZ[i];
+  }
+  _nrClusters += selected_k;
+}
---
+++ src/precluster.h
@@ -0,0 +1,87 @@
+/*
+ * precluster.h
+ *
+ *  Created on: 6-jun-2019
+ *      Author: M. El-Kebir
+ */
+
+#ifndef PRECLUSTER_H
+#define PRECLUSTER_H
+
+#include "utils.h"
+#include "readmatrix.h"
+#include "solver.h"
+
+#ifdef CPLEX
+  #include "hardpreclusterilpcplex.h"
+#else
+#endif
+
+class PreCluster
+{
+public:
+  
+#ifdef CPLEX
+  typedef HardPreClusterIlpCplex HardPreClusterIlpAlg;
+#endif
+  
+  /// Constructor
+  ///
+  /// @param R Read matrix
+  PreCluster(const ReadMatrix& R);
+  
+  /// Run preclustering
+  ///
+  /// @param nrSegments Number of segments for piecewise linear approximation
+  /// @param statType Summary statistic to use for clustering
+  /// @param precisionBetaBin Precision parameter for beta binomial
+  /// @param nrThreads Number of threads
+  /// @param timeLimit Time limit in seconds (unlimited if -1)
+  /// @param verbose Verbose
+  /// @param memoryLimit Memory limit
+  void run(int nrSegments,
+           Solver::ClusterStatisticType statType,
+           double precisionBetaBin,
+           int nrThreads,
+           int timeLimit,
+           bool verbose,
+           int memoryLimit);
+  
+  /// Return pre clustering
+  const IntMatrix& getPreClustering() const
+  {
+    return _invPreClustering;
+  }
+  
+protected:
+  /// Run preclustering for specified cluster
+  ///
+  /// @param snvIndices SNV indices that comprise cluster
+  /// @param nrSegments Number of segments for piecewise linear approximation
+  /// @param statType Summary statistic to use for clustering
+  /// @param precisionBetaBin Precision parameter for beta binomial
+  /// @param nrThreads Number of threads
+  /// @param timeLimit Time limit in seconds (unlimited if -1)
+  /// @param verbose Verbose
+  /// @param memoryLimit Memory limit
+  void run(const IntVector& snvIndices,
+           int nrSegments,
+           Solver::ClusterStatisticType statType,
+           double precisionBetaBin,
+           int nrThreads,
+           int timeLimit,
+           bool verbose,
+           int memoryLimit);
+  
+protected:
+  /// Read matrix
+  const ReadMatrix& _R;
+  /// Number of clusters
+  int _nrClusters;
+  /// Pre clustering
+  IntVector _preClustering;
+  /// Inverse pre clustering
+  IntMatrix _invPreClustering;
+};
+
+#endif // PRECLUSTER
